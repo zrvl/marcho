f0994cab32bf69da792629c3c1f91a93d38db433
zrvl <zhuravelbogdan0@gmail.com>
2022-01-26 12:33:51 +0200

    added footer

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,30 @@ const uglify =       require('gulp-uglify');
 const imagemin =     require('gulp-imagemin');
 const del =          require('del');
 const browserSync =  require('browser-sync').create();
+const svgSprite = require('gulp-svg-sprite');
+const cheerio = require('gulp-cheerio');
+const replace = require('gulp-replace');
+
+function svgSprites() {
+  return src('app/images/icons/*.svg')
+  .pipe(cheerio({
+    run: ($) => {
+      $("[fill]").removeAttr("fill");
+      $("[stroke]").removeAttr("stroke");
+      $("[style]").removeAttr("style");
+    },
+    parserOptions: {xmlMode: true},
+  }))
+  .pipe(replace('&gt;','>'))
+  .pipe(svgSprite({
+    mode: {
+      stack: {
+        sprite: '../sprite.svg'
+      },
+    },
+  }))
+  .pipe(dest('app/images'));
+}
 
 function browsersync() {
   browserSync.init({
@@ -72,6 +96,7 @@ function watching() {
   watch(['app/scss/**/*.scss'], styles);
   watch(['app/js/**/*.js', '!app/js/main.min.js'], scripts);
   watch(['app/**/*.html']).on('change', browserSync.reload);
+  watch(['app/images/icons/*.svg'], svgSprites);
 }
 
 exports.styles = styles;
@@ -80,6 +105,7 @@ exports.browsersync = browsersync;
 exports.watching = watching;
 exports.images = images;
 exports.cleanDist = cleanDist;
+exports.svgSprites = svgSprites;
 exports.build = series(cleanDist, images, build);
 
-exports.default = parallel(styles, scripts, browsersync, watching);
+exports.default = parallel(svgSprites, styles, scripts, browsersync, watching);
