c1e3d315d5329b0ff01a4775c65f05ba322d4d0e
zrvl <zhuravelbogdan0@gmail.com>
2022-01-28 16:22:05 +0200

    added product

--- app/js/main.js
+++ app/js/main.js
@@ -5,5 +5,13 @@ $(function() {
     fade: true,
     autoplay: true,
     autoplaySpeed: 2000,
-  })
-})
+  });
+
+  $('.star').rateYo({
+    starWidth: "17px",
+    normalFill: "#ccccce",
+    ratedFill: "#ffc35b",
+    readOnly: true,
+  });
+
+});
